abfb8c55c68abc1df134fea878eeeab52434a78d
Parker Parham <parkerparham@gmail.com>
2022-10-04 21:46:22 -0400

    Added navbar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./NavBar";
+import "../App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;

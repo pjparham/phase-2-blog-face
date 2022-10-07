25353a71dfcaa150fb71c7e820d929ea96945fb5
Parker Parham <parkerparham@gmail.com>
2022-10-07 17:22:10 -0400

    Updated About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,9 +4,10 @@ import { AboutContainer } from "./AboutElements"
 
 function About(){
     return (
-        <div>
-            <AboutContainer style={{ lineHeight : 1.3 }} className="about">BlogFace is a platform for users to share what they are passionate about in a longer form. We encourage users to share their knowledge and experiences to enrich the community. If you haven't please make an account to see what BlogFace has to offer.</AboutContainer>
-        </div>
+        <AboutContainer>
+            <h3>About BlogFace</h3>
+            <p>BlogFace is a platform for users to share what they are passionate about in a longer form. We encourage users to share their knowledge and experiences to enrich the community. If you haven't already, please make an account to see what BlogFace has to offer.</p>
+        </AboutContainer>
     )
 }
 
--- src/components/About/AboutElements.js
+++ src/components/About/AboutElements.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
-export const AboutContainer = styled.h3`
+export const AboutContainer = styled.div`
     margin: 1rem 9rem;
     padding: 1rem;
     text-align: left;
     background: #ebebeb;
     border-radius: 15px;
+    line-height: 1.3;
     @media screen and (max-width: 768px) {
         width: 80%;
         margin: 1rem;

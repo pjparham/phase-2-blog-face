f901bab995678655824fd9591ab351f232554bff
Parker Parham <parkerparham@gmail.com>
2022-10-10 12:27:41 -0400

    Users can like posts

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -22,6 +22,29 @@ function BlogPost({ post, user, handleUpdatePost, handleDeletePost }){
           .then(() => handleDeletePost(post));
       }
 
+      console.log(user.sub)
+
+      function handleLike(e){
+        e.preventDefault();
+        if (post.likes.includes(user.sub)){
+            return null
+        }
+        else{
+            const updatedPost = {
+                "likes": [...post.likes, user.sub]
+            }
+            fetch(`http://localhost:3004/posts/${post.id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(updatedPost)
+            })
+            .then((r) => r.json())
+            .then((updatedPost) => handleUpdatePost(updatedPost))
+        }
+    }
+
     return (
         <PostContainer>
             <TitleContainer>
@@ -40,6 +63,7 @@ function BlogPost({ post, user, handleUpdatePost, handleDeletePost }){
             {post.userName ? <UserName>By: {post.userName}</UserName> : null}
             <h4>{subhead}</h4>
             <Link to={`/posts/${post.id}`}>See more</Link>
+            <button onClick={handleLike}>{post.likes? (post.likes.length) + "Likes" : "Like"}</button>
             { edit ? <EditPost setEdit={setEdit} handleUpdatePost={handleUpdatePost} post={post} /> : null}
         </PostContainer>
     )
